2c780be764b5ab1ba95c370820ae9c8caa695e8c
Chunchun Dan <chunchundan@Chunchuns-MacBook-Air.local>
2020-02-25 21:36:32 +1100

    build a single component of comment

--- src/index.js
+++ src/index.js
@@ -2,17 +2,29 @@ import React from 'react';
 import reactDOM from 'react-dom';
 
 const App = () => {
-    const buttonText = 'Click Here';
-    const style = {backgroundColor: 'green', color: 'white'};
-
-    return <div>
-        <label className="label" htmlFor="name">Enter Name:</label>
-        <input id="name" type="text"/>
-        <button style={style}>{buttonText}</button>
-    </div>
-}
+    return (
+        <div className="ui container comments">
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="avatar" />
+                </a>        
+                <div className="content">
+                    <a href="/" className="author">
+                        Chunchun
+                    </a>
+                    <div className="metadata">
+                        <span className="date">Today at 9:32PM</span>
+                    </div>
+                    <div className="text">
+                        leave comment here.
+                    </div>
+                </div>
+            </div>     
+        </div>
+    );
+};
 
 reactDOM.render(
     <App />,
     document.querySelector('#root')
-)
+);
