d5eb30e0c32d271f0b82484e567f3b05056402d4
Chunchun Dan <chunchundan@Chunchuns-MacBook-Air.local>
2020-02-25 21:50:33 +1100

    add faker image avatar

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import reactDOM from 'react-dom';
+import faker from 'faker';
 
 const App = () => {
     return (
         <div className="ui container comments">
             <div className="comment">
                 <a href="/" className="avatar">
-                    <img alt="avatar" />
+                    <img alt="avatar" src={faker.image.avatar()} />
                 </a>        
                 <div className="content">
                     <a href="/" className="author">
-                        Chunchun
+                        {faker.name.jobTitle()}
                     </a>
                     <div className="metadata">
                         <span className="date">Today at 9:32PM</span>
