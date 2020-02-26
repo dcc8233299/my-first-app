53691b199ef6e51abc93f810da8c201c5bdea9e1
Dan, Chunchun <>
2020-02-26 18:06:44 +1100

    refactoring

--- src/index.js
+++ src/index.js
@@ -6,9 +6,24 @@ import CommentDetail from './CommentDetail';
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail avatar={faker.image.avatar()} author="Sam" timeAgo="Yesterday at 2:00AM" commentText="Very good!" />
-            <CommentDetail avatar={faker.image.avatar()} author="Alex" timeAgo="Today at 4:54PM" commentText="Actually it's a bit boring." />
-            <CommentDetail avatar={faker.image.avatar()} author="Jane" timeAgo="Today at 8:00PM" commentText="I like this!" />
+            <CommentDetail 
+                avatar={faker.image.avatar()} 
+                author="Sam" 
+                timeAgo="Yesterday at 2:00AM" 
+                commentText="Very good!" 
+            />
+            <CommentDetail 
+                avatar={faker.image.avatar()} 
+                author="Alex" 
+                timeAgo="Today at 4:54PM" 
+                commentText="Actually it's a bit boring." 
+            />
+            <CommentDetail 
+                avatar={faker.image.avatar()} 
+                author="Jane" 
+                timeAgo="Today at 8:00PM" 
+                commentText="I like this!" 
+            />
         </div>
     );
 };
