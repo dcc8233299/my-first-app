a419e6d0dc07c06431f63b8d07a54c1fb80d4715
Chunchun Dan <chunchundan@Chunchuns-MacBook-Air.local>
2020-02-26 23:44:02 +1100

    parsing the children component to a component

--- src/index.js
+++ src/index.js
@@ -7,25 +7,32 @@ import ApprovalCard from './ApprovalCard';
 const App = () => {
     return (
         <div className="ui container comments">
-            <ApprovalCard />
-            <CommentDetail 
-                avatar={faker.image.avatar()} 
-                author="Sam" 
-                timeAgo="Yesterday at 2:00AM" 
-                commentText="Very good!" 
-            />
-            <CommentDetail 
-                avatar={faker.image.avatar()} 
-                author="Alex" 
-                timeAgo="Today at 4:54PM" 
-                commentText="Actually it's a bit boring." 
-            />
-            <CommentDetail 
-                avatar={faker.image.avatar()} 
-                author="Jane" 
-                timeAgo="Today at 8:00PM" 
-                commentText="I like this!" 
-            />
+            <ApprovalCard>
+                <CommentDetail 
+                    avatar={faker.image.avatar()} 
+                    author="Sam" 
+                    timeAgo="Yesterday at 2:00AM" 
+                    commentText="Very good!" 
+                />
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail 
+                    avatar={faker.image.avatar()} 
+                    author="Alex" 
+                    timeAgo="Today at 4:54PM" 
+                    commentText="Actually it's a bit boring." 
+                />
+            </ApprovalCard>
+            
+            <ApprovalCard>
+                <CommentDetail 
+                    avatar={faker.image.avatar()} 
+                    author="Jane" 
+                    timeAgo="Today at 8:00PM" 
+                    commentText="I like this!" 
+                />
+            </ApprovalCard>
         </div>
     );
 };
